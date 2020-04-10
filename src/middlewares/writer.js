08db1f8308daba880518957737bec9daa02f357c
Yunus Emre <yek.plus.plus@gmail.com>
2020-04-10 03:48:41 +0300

    writer updated

--- src/middlewares/writer.js
+++ src/middlewares/writer.js
@@ -1,5 +1,15 @@
 const fs = require('fs')
+const path = require('path')
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
 const writer = (file,data) => {
+  ensureDirectoryExistence(file)
   fs.writeFileSync(`${file}.json`, JSON.stringify(data));
 }
 
